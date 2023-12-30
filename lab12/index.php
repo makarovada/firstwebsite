9b8286c6afef1b8c4c1cbc284609d9cce1aa6da9
Дарья <makarovada.12345@gmail.com>
2023-12-30 09:48:28 +0300

    all lab

---
+++ lab12/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mospolytechfamily</title>
+    <link href="bootstrap.min.css" rel="stylesheet">
+    <title>Макарова Дарья Антоновна 221-362 - Лабораторная работа №12</title>
+</head>
+
+<body>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-12 col-md-6 col-lg-5">
+        <h1>Moscow Polytech family</h1>
+        <p>Игра-новелла о маскотах факультетов Московского политеха</p>
+      </div>
+      <div class="col-12 col-md-6 col-lg-7">
+        <img src="image.jpg" alt="Изображение проекта" class="img-fluid">
+      </div>
+    </div>
+  </div>
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-12 col-md-6 col-lg-3">
+        <h2>Основные особенности</h2>
+      </div>
+      <div class="col-12 col-md-6 col-lg-7">
+      <ul>
+          <li>Новелла - популярный жанр компьютерных игр, позволяющий донести информацию в интерактивной форме.</li>
+          <li>Уникальные персонажи позоволяет с первых секунд ощущать особенности каждого факультета.</li>
+          <li>В игре доступна только актуальная информация о приемной компании и факультетах, что делает игру достоверным источником информации.</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-12 col-md-6 col-lg-5">
+        <h2>Свяжитесь с нами</h2><br>
+        <p>mospolytechfamily@mail.ru</p>
+        <p>+7 (495) 837-60-39</p>
+      </div>
+      <div class="col-12 col-md-6 col-lg-7">
+      <form>
+          <div class="mb-3">
+            <label for="name" class="form-label">Ваше имя</label>
+            <input type="text" class="form-control" id="name">
+          </div>
+          <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email">
+          </div>
+          <div class="mb-3">
+            <label for="message" class="form-label">Сообщение</label>
+            <textarea class="form-control" id="message" rows="3"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Отправить</button>
+        </form>
+      </div>
+    </div>
+  </div>
+  <br>
+  <div class="d-grid gap-2">
+  <a href="https://www.figma.com/proto/bSREYyEj7s8nRv242LPnp4/APP_PD?type=design&node-id=109-4272&scaling=scale-down&page-id=0%3A1" class="btn btn-lg btn-primary" target="_blank">Играть</a>
+</div>
+
+
+</body>
+</html>
